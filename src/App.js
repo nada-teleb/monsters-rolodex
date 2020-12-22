41493c7fb3fdf5a8c4db5b5220e8457566c478e1
nada-teleb <telebnada@gmail.com>
2020-12-22 16:32:45 +0200

    Upload monsters-rolodex app

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import './App.css';
+
+class App extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            monsters: [],
+            searchText: ""
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({monsters: users}))
+    }
+
+    handleChange = e => {
+        this.setState({ searchText: e.target.value });
+    }
+
+    render() {
+        const { monsters, searchText } = this.state;
+        const filteredMonsters = monsters.filter(monster => 
+            monster.name.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        return (
+          <div className="App">
+            <h1> Monsters Rolodex </h1>
+            <SearchBox placeholder="search monsters" handleChange={this.handleChange} />
+            <CardList monsters={filteredMonsters} />
+          </div>
+        );
+    }
+}
+
+export default App;
